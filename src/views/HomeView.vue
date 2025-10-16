2ba4d8ad0c47d69dacab2f8fef52a0e213d4336f
loganrenz <narduk@mac.com>
2025-10-16 11:18:31 -0500

    ok

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,7 +13,6 @@
     <AppFooter />
   </div>
 </template>
-
 <script setup lang="ts">
 import AppHeader from '@/components/layout/AppHeader.vue'
 import AppFooter from '@/components/layout/AppFooter.vue'
