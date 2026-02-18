7c52eec39dc3a41ba13beb031a0042a8182f0e1b
pacc-services <marketing@pacc.services>
2026-02-18 10:51:58 -0700

    Add k2HydrogenFttCoverage to articles array

--- src/data/articles/index.ts
+++ src/data/articles/index.ts
@@ -1,7 +1,8 @@
 import type { Article } from '@/types/article'
 import { k2HydrogenFacility } from './k2-hydrogen-facility'
+import { k2HydrogenFttCoverage } from './k2-hydrogen-ftt-coverage'
 
-export const articles: Article[] = [k2HydrogenFacility]
+export const articles: Article[] = [k2HydrogenFacility, k2HydrogenFttCoverage]
 
 export const getArticleBySlug = (slug: string): Article | undefined => {
   return articles.find((article) => article.slug === slug)
