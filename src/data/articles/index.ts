e1c820a9ae7c005eb8729a8745269f50da04c4bf
pacc-services <marketing@pacc.services>
2026-06-17 08:08:33 -0700

    Register Transit Tomorrow article in index

--- src/data/articles/index.ts
+++ src/data/articles/index.ts
@@ -3,8 +3,9 @@ import { k2HydrogenFacility } from './k2-hydrogen-facility'
 import { k2HydrogenFttCoverage } from './k2-hydrogen-ftt-coverage'
 import { hysterYaleHydrocharge } from './hyster-yale-hydrocharge'
 import { zeroUpEnergyHsa } from './zeroup-energy-hsa'
+import { transitTomorrowHydrogen } from './transit-tomorrow-hydrogen'
 
-export const articles: Article[] = [k2HydrogenFacility, k2HydrogenFttCoverage, hysterYaleHydrocharge, zeroUpEnergyHsa]
+export const articles: Article[] = [k2HydrogenFacility, k2HydrogenFttCoverage, hysterYaleHydrocharge, zeroUpEnergyHsa, transitTomorrowHydrogen]
 
 export const getArticleBySlug = (slug: string): Article | undefined => {
   return articles.find((article) => article.slug === slug)
